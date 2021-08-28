319b6d891942e64d41cf399f8b934899caf33b96
unknown <raveen.anand@whitehatjr.com>
2021-08-28 15:23:49 +0530

    First

---
+++ Best Friends Array/sketch.js
@@ -0,0 +1,24 @@
+var friends =["Ria","Jyoti","Raj","Ritu","Avi"];
+
+function setup() {
+  //size of the canvas to draw on
+  createCanvas(innerWidth, innerHeight);
+
+  // Set the background to white, and move the line to setup();
+  background("white");
+  
+console.log("My best friends are :");
+
+ for (i=0; i < friends.length; i++){
+    console.log(friends[i]);
+  }
+
+}
+
+
+function draw() {
+    
+}
+
+    
+
